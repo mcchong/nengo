9c9ab67249d8bbb526e190b51bacc9d59d15b8ee
Terry Stewart <tcstewar@uwaterloo.ca>
2013-10-28 16:45:44 -0400

    Added networks.Array as a more explicit form of EnsembleArray

--- nengo/networks/__init__.py
+++ nengo/networks/__init__.py
@@ -1,4 +1,5 @@
 from .network import Network
+from .array import Array
 from .basalganglia import BasalGanglia
 from .circularconvolution import CircularConvolution
 from .integrator import Integrator
